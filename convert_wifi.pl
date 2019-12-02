2c04e170e06fd87a69d64f41cc66a5109601a92b
Matija Nalis <mnalis-git@voyager.hr>
2019-12-02 21:37:52 +0100

    boilerplate

---
+++ convert_wifi.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+# by Matija Nalis <mnalis-perl@voyager.hr>, Apache 2.0 license, started 2019-12-02
+#
+# converts android WiFi passwords from old wpa_supplicant.conf to newer WifiConfigStore.xml
+#
+
+use warnings;
+use strict;
+use autodie;
+
