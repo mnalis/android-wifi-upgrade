0beb519a1745a6f193284bfca7d0bcf7c76fec02
Matija Nalis <mnalis-git@voyager.hr>
2019-12-02 23:00:14 +0100

    initial (fake) XML support

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # android-wifi-upgrade
 convert WiFi passwords from old Android wpa_supplicant.conf to newer (post-Oreo) WifiConfigStore.xml
+
+## Description
+For me, upgrading LG G3 from LineageOS 14.1 (Android 7.1.2 "Nougat") to LineageOS 16.0 (Android 9 "Pie") resulted 
+in all my wifi password being lost. The reason is the information is no longer kept in 
+"/data/misc/wifi/wpa_supplicant.conf" but instead in "/data/misc/wifi/WifiConfigStore.xml" 
+
+This script is a quick hack to convert (most of the) passwords to new format
+so I don't have to remember and type them all again.
+
+## Usage
+This perl script reads old wpa_supplicant.conf on STDIN, and outputs new WifiConfigStore.xml to STDOUT
+
+## TODO
+* Never write XML by hand as I do here!  It will seem to work in most cases, and then break badly on some non-escaped value or similar.
+* only config I implemented currently are open networks (no PSK) and WPA2 networks (with ASCII PSK). Maybe support other (WEP, WPA1, EAP-xxx thingies) ?
+* FIXMEs in code
--- convert_wifi.pl
+++ convert_wifi.pl
@@ -11,13 +11,130 @@ use Data::Dumper;
 
 my $DEBUG = $ENV{DEBUG} || 0;
 my $IGN_ERR = $ENV{IGN_ERR} || 0;
+$| = 1;
 
 sub dbg($$) {
 	my ($lvl, $msg) = @_;
-	print "dbg:$lvl" . (' ' x ($lvl*1)) . "$msg\n" if $DEBUG >= $lvl;
+	print STDERR "dbg:$lvl" . (' ' x ($lvl*1)) . "$msg\n" if $DEBUG >= $lvl;
 }
 
 my %CUR=();
+
+# header of WifiConfigStore.xml
+sub start_xml() {
+	print <<EOF
+<?xml version="1.0" encoding="utf-8" standalone="yes"?>
+<WifiConfigStoreData>
+    <int name="Version" value="1"/>
+    <NetworkList>
+
+EOF
+}
+
+# FIXME: we should never do XML by hand like this. oh well.
+sub quote_xml($) {
+	my ($str) = @_;
+	$str =~ s{\"}{&quot;}g;
+	return $str;
+}
+
+# constructs one network entry in WifiConfigStore.xml
+sub add_xml() {
+
+	if ($CUR{disabled} eq 1) {
+		warn "ignoring disabled network $CUR{ssid}";
+		return;
+	}
+
+	print q{
+        <Network>
+            <WifiConfiguration>};
+
+	my $ConfigKey = quote_xml 'FIXME NETNAME_WPA_PSK';
+	my $SSID = quote_xml '"FIXME_SSID_WITH_QUOTES"';
+	my $CreationTime = 'time=12-01 00:00:00.000';	# FIXME example: time=12-02 01:47:38.625
+	my $CreationUID = '1000';	# FIXME make it user configurable?
+
+
+	my $PSK_LINE = '<null name="PreSharedKey"/>';
+	my $AllowedKeyMgmt = '01';	# 01 for null PSK, 02 otherwise?
+	if ($CUR{key_mgmt} ne 'NONE') {
+		my $PreSharedKey = quote_xml '"FIXME_UNDEF_OR_QUOTED_PSK"';
+	}
+	
+	print qq{
+                <string name="ConfigKey">$ConfigKey</string>
+                <string name="SSID">$SSID</string>
+                <null name="BSSID"/>
+                $PSK_LINE
+                <null name="WEPKeys"/>
+                <int name="WEPTxKeyIndex" value="0"/>
+                <boolean name="HiddenSSID" value="false"/>
+                <boolean name="RequirePMF" value="false"/>
+                <byte-array name="AllowedKeyMgmt" num="1">$AllowedKeyMgmt</byte-array>
+                <byte-array name="AllowedProtocols" num="1">03</byte-array>
+                <byte-array name="AllowedAuthAlgos" num="1">01</byte-array>
+                <byte-array name="AllowedGroupCiphers" num="1">0f</byte-array>
+                <byte-array name="AllowedPairwiseCiphers" num="1">06</byte-array>
+                <boolean name="Shared" value="true"/>
+                <int name="Status" value="2"/>
+                <null name="FQDN"/>
+                <null name="ProviderFriendlyName"/>
+                <null name="LinkedNetworksList"/>
+                <null name="DefaultGwMacAddress"/>
+                <boolean name="ValidatedInternetAccess" value="false"/>
+                <boolean name="NoInternetAccessExpected" value="false"/>
+                <int name="UserApproved" value="0"/>
+                <boolean name="MeteredHint" value="false"/>
+                <int name="MeteredOverride" value="0"/>
+                <boolean name="UseExternalScores" value="false"/>
+                <int name="NumAssociation" value="0"/>
+                <int name="CreatorUid" value="$CreationUID"/>
+                <string name="CreatorName">android.uid.system:$CreationUID</string>
+                <string name="CreationTime">$CreationTime</string>
+                <int name="LastUpdateUid" value="$CreationUID"/>
+                <string name="LastUpdateName">android.uid.system:$CreationUID</string>
+                <int name="LastConnectUid" value="0"/>
+                <boolean name="IsLegacyPasspointConfig" value="false"/>
+                <long-array name="RoamingConsortiumOIs" num="0"/>
+                <string name="RandomizedMacAddress">02:00:00:00:00:00</string>
+};
+	
+	die;
+	
+	print q{            </WifiConfiguration>
+            <NetworkStatus>
+                <string name="SelectionStatus">NETWORK_SELECTION_ENABLED</string>
+                <string name="DisableReason">NETWORK_SELECTION_ENABLE</string>
+                <null name="ConnectChoice"/>
+                <long name="ConnectChoiceTimeStamp" value="-1"/>
+                <boolean name="HasEverConnected" value="false"/>
+            </NetworkStatus>
+            <IpConfiguration>
+                <string name="IpAssignment">DHCP</string>
+                <string name="ProxySettings">NONE</string>
+            </IpConfiguration>
+        </Network>
+};
+}
+
+# footer of WifiConfigStore.xml
+sub end_xml() {
+	print q{
+
+    </NetworkList>
+    <PasspointConfigData>
+        <long name="ProviderIndex" value="0"/>
+    </PasspointConfigData>
+</WifiConfigStoreData>
+}
+}
+
+#
+# here goes the main loop
+#
+
+start_xml();
 while (<STDIN>)
 {
 	chomp;
@@ -34,7 +151,7 @@ while (<STDIN>)
 		if (!defined $CUR{'ssid'}) {
 			warn "can't parse network without SSID, ignoring block:" . Dumper(\%CUR);
 		}
-		die;
+		add_xml();
 		%CUR=();
 		next;
 	}
@@ -45,3 +162,5 @@ while (<STDIN>)
 		warn "ignoring unparseable line: $_";
 	}
 }
+
+end_xml();
